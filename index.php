0a14364f8bf2002d5153ec8b56e5ca6266819bde
CFTheMaster <hentaimasterpad@gmail.com>
2020-02-29 17:32:49 +0100

    see if this works

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <html>
 <head>
-
-    <title>compterfreaker</title>
+    <title>computerfreaker</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 
     <!-- Meta Data -->
-    <meta name="theme-color" content="#14bae4">
+    <meta name="theme-color" content="#6600cc">
     <meta content="https://computerfreaker.cf/profile/profile.png" property="og:image" />
-    <link rel="icon" type="image/x-icon" href="https://computerfreaker.cf/profile/profile.png">
+    <link rel="icon" type="image/x-icon" href="http://computerfreaker.cf/profile/profile.png">
     <meta content="computerfreaker" property="og:title" />
 
     <meta content="Homepage" property="og:description" />
     <meta content="Homepage" name="description" />
 
     <meta name="revisit-after" content="2 days">
-    <meta name="keywords" content="computerfreaker, Twitter, CFsAPI, Github, Discord">
+    <meta name="keywords" content="computerfreaker, Twitter, Developer, Github, Discord">
 
-    <meta property="og:locale" content="en_GB" />
+    <meta property="og:locale" content="en_US" />
     <link rel="canonical" href="https://computerfreaker.cf/" />
     <meta property="og:url" content="https://computerfreaker.cf/" />
     <meta property="og:site_name" content="computerfreaker" />
@@ -36,6 +35,7 @@
 </head>
 <body class="github-font-standard">
 <div class="fullscreen">
+    <div class="background" style="background-image: url('/bg/background.png');"></div>
     <div class="center-object">
         <div class="container">
             <section class="me unset">
@@ -44,32 +44,37 @@
                 </div>
                 <div class="unset">
                     <h1 class="title monospace">computerfreaker</h1>
-                    <p class="undertitle monospace">AKA CF</p>
+                    <p class="undertitle monospace">aka CF</p>
                 </div>
             </section>
             <div class="buttons">
                 <a href="https://discord.gg/DDRbw7W" class="btn discord-dropshadow animation hover" target="_blank">Discord</a>
                 <a href="https://github.com/CFTheMaster" class="btn github-dropshadow animation hover" target="_blank">GitHub</a>
                 <a href="https://twitter.com/computerfreaker" class="btn twitter-dropshadow animation hover" target="_blank">Twitter</a>
+                <a href="https://twitch.tv/computerfreaker" class="btn twitter-dropshadow animation hover" target="_blank">Twitch</a>
+                <a href="https://mixer.com/computerfreaker" class="btn twitter-dropshadow animation hover" target="_blank">Mixer</a>
                 <a href="mailto:computerfreaker@computerfreaker.cf" class="btn emerald-dropshadow animation hover">Email</a>
             </div>
         </div>
     </div>
+
+    <a href="#information"><div id="fade" class="arrow bounce">
+        </div></a>
 </div>
 
 <section class="container" id="information">
     <div class="box-container">
         <h3 class="title emerald-text"><i class="emoji blobcatnerd"></i> Information:</h3>
         <div class="info-container"><p>
-                hello there <span class="emerald-text">friends</span>, Welcome to my homepage.
+                Hello <span class="emerald-text">friends</span>, welcome to my website, my name is Emillia, I am just a simple coder
             </p></div>
     </div>
 
     <div class="box-container">
         <h3 class="title emerald-text"><i class="emoji blobcatbusiness"></i> Projects:</h3>
         <div class="info-container">
-            <p><a href="https://api.computerfreaker.cf" >My API</a>, a simple API that I made</p>
-            <p><a href="https://uni.computerfreaker.cf" >Uni Bot</a>, a bot I made in Kotlin</p>
+            <p><a href="https://api.computerfreaker.cf">API</a>, a simple API I created</p>
+            <p><a href="https://github.com/CFTheMaster/Unibot" target="_blank">Uni Bot</a>, a bot I made in Kotlin</p>
         </div>
     </div>
 </section>
