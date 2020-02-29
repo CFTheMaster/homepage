6d9df7a3a49ba5294760aff9c0d00bbb1d27ecb5
CFTheMaster <hentaimasterpad@gmail.com>
2020-02-29 16:59:49 +0100

    make a homepage

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+
+    <title>compterfreaker</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+
+    <!-- Meta Data -->
+    <meta name="theme-color" content="#14bae4">
+    <meta content="https://computerfreaker.cf/profile/profile.png" property="og:image" />
+    <link rel="icon" type="image/x-icon" href="https://computerfreaker.cf/profile/profile.png">
+    <meta content="computerfreaker" property="og:title" />
+
+    <meta content="Homepage" property="og:description" />
+    <meta content="Homepage" name="description" />
+
+    <meta name="revisit-after" content="2 days">
+    <meta name="keywords" content="computerfreaker, Twitter, CFsAPI, Github, Discord">
+
+    <meta property="og:locale" content="en_GB" />
+    <link rel="canonical" href="https://computerfreaker.cf/" />
+    <meta property="og:url" content="https://computerfreaker.cf/" />
+    <meta property="og:site_name" content="computerfreaker" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@computerfreaker" />
+
+    <!-- CSS -->
+    <link href="https://modesta.alexflipnote.dev/css/modesta.css" type="text/css" rel="stylesheet">
+    <link href="https://modesta.alexflipnote.dev/css/blobcat.css" type="text/css" rel="stylesheet">
+</head>
+<body class="github-font-standard">
+<div class="fullscreen">
+    <div class="center-object">
+        <div class="container">
+            <section class="me unset">
+                <div class="avatar-container">
+                    <img draggable="false" src="/profile/profile.png" class="image-title">
+                </div>
+                <div class="unset">
+                    <h1 class="title monospace">computerfreaker</h1>
+                    <p class="undertitle monospace">AKA CF</p>
+                </div>
+            </section>
+            <div class="buttons">
+                <a href="https://discord.gg/DDRbw7W" class="btn discord-dropshadow animation hover" target="_blank">Discord</a>
+                <a href="https://github.com/CFTheMaster" class="btn github-dropshadow animation hover" target="_blank">GitHub</a>
+                <a href="https://twitter.com/computerfreaker" class="btn twitter-dropshadow animation hover" target="_blank">Twitter</a>
+                <a href="mailto:computerfreaker@computerfreaker.cf" class="btn emerald-dropshadow animation hover">Email</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="container" id="information">
+    <div class="box-container">
+        <h3 class="title emerald-text"><i class="emoji blobcatnerd"></i> Information:</h3>
+        <div class="info-container"><p>
+                hello there <span class="emerald-text">friends</span>, Welcome to my homepage.
+            </p></div>
+    </div>
+
+    <div class="box-container">
+        <h3 class="title emerald-text"><i class="emoji blobcatbusiness"></i> Projects:</h3>
+        <div class="info-container">
+            <p><a href="https://api.computerfreaker.cf" >My API</a>, a simple API that I made</p>
+            <p><a href="https://uni.computerfreaker.cf" >Uni Bot</a>, a bot I made in Kotlin</p>
+        </div>
+    </div>
+</section>
+
+<footer class="center box-container primary no-border-radius">
+    <p class="no-margin">
+        <a href="https://github.com/AlexFlipnote/alexflipnote.github.io/blob/master/LICENSE" target="_blank">LICENSE</a>
+        | Thanks to AlexFlipnote for the framework|
+        <a href="https://github.com/AlexFlipnote/alexflipnote.github.io" target="_blank">Source</a>
+    </p>
+</footer>
+
+</body>
+</html>
