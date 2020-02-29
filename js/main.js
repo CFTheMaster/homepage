23b30acb542ee97a6d0076895cb45ce36a8b708d
CFTheMaster <hentaimasterpad@gmail.com>
2020-02-29 17:18:02 +0100

    woosh

--- js/main.js
+++ js/main.js
@@ -17,18 +17,6 @@ function getParameterByName(name, url) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Load the time since I coded
-    var years_since = new Date("2015-08-01").getTime();
-    var today = new Date().getTime();
-
-    var ms = today - years_since;
-    var m = Math.floor(ms / 60000);
-    var h = Math.floor(m / 60);
-    var d = Math.floor(h / 24);
-    var y = Math.floor(d / 365);
-
-    document.getElementById("years-of-coding").innerText = y + " years";
-
     if (getParameterByName('old')) {
         document.querySelector('head').innerHTML += '<link href="/css/classic.css" type="text/css" rel="stylesheet">';
     }
